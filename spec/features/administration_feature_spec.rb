9a8abc2bfa770c244b7abf2e12b33d24f8b86957
Gerard Morera <grdmorera@gmail.com>
2015-09-20 12:22:43 +0100

    test one more than category set up

--- spec/features/administration_feature_spec.rb
+++ spec/features/administration_feature_spec.rb
@@ -7,7 +7,7 @@ feature 'administrator management' do
     expect(page).to have_content('no products')
   end
 
-  scenario 'administrator insert products' do
+  scenario 'administrator insert a product' do
     visit '/admin/products'
 
     click_link 'insert new product'
@@ -18,5 +18,20 @@ feature 'administrator management' do
     click_button 'submit'
 
     expect(page).to have_content('Headphones')
+  end
+
+  scenario 'administrator insert a product with 2 categories' do
+    visit '/admin/products'
+
+    click_link 'insert new product'
+
+    fill_in 'name',        with: "cotton-sheet"
+    fill_in 'description', with: "cotton, double bed"
+    fill_in 'category',    with: "home"
+    fill_in 'category',    with: "cloth"
+    click_button 'submit'
+
+    expect(page).to have_content('home')
+    expect(page).to have_content('cloth')
   end
 end
