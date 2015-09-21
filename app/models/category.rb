e5f9cff120bb56d067cbcba5e7c4fd6d7a41fbef
Gerard Morera <grdmorera@gmail.com>
2015-09-21 11:56:51 +0100

    self relation working

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :products, through: :products_categories
   has_many :products_categories
+  has_many :subcategories, through: :categorizations
+  has_many :categorizations
 end
