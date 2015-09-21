5b278130bb18b33e29c7d4b5739b7a6638b6c81b
Gerard Morera <grdmorera@gmail.com>
2015-09-21 20:39:42 +0100

    first refactor

---
+++ app/services/categories/create.rb
@@ -0,0 +1,37 @@
+module Categories
+  class Create
+    def initialize params
+      @params = params
+    end
+
+    def call
+      (category = create category_params) && category.subcategories(create subcategory_params)
+    end
+
+    private
+
+    attr_reader :params
+
+    def create category_params
+      if has_value? category_params
+        Category.find_or_create_by category_params
+      else
+        nil
+      end
+    end
+    
+    def category_params 
+      params.require(:category).permit(:title)
+    end
+
+    def subcategory_params
+      @subcategory_params ||= params.require(:category).permit(:subcategory => :title).values.first
+    end
+
+    def has_value? category_params
+      !category_params.values.first.empty?
+    end
+  end
+end
+
+# (subcategory_params.values.first)
