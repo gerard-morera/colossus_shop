18ccd0b9e3d14aaa0e9d45d733448b162e16abfe
Gerard Morera <grdmorera@gmail.com>
2015-09-21 22:23:01 +0100

    posponing refactor

--- app/services/categories/create.rb
+++ app/services/categories/create.rb
@@ -1,33 +1,35 @@
 module Categories
   class Create
-    def initialize params, creatable_class: Categories::Creatable
-      @params          = params
-      @creatable_class = creatable_class
+    def initialize params
+      @params = params
     end
 
     def call
-      category    = create(creatable(category_params))
-      subcategory = category.subcategories(create(creatable subcategory_params))
+      (category = create category_params) && category.subcategories(create subcategory_params)
     end
 
     private
 
-    attr_reader :params, :creatable_class
+    attr_reader :params
 
     def create category_params
-      Category.find_or_create_by category_params
+      if has_value? category_params
+        Category.find_or_create_by category_params
+      else
+        nil
+      end
     end
-
-    def creatable params
-      creatable_class.new(params).call
-    end
-
+    
     def category_params 
       params.require(:category).permit(:title)
     end
 
     def subcategory_params
       @subcategory_params ||= params.require(:category).permit(:subcategory => :title).values.first
+    end
+
+    def has_value? category_params
+      !category_params.values.first.empty?
     end
   end
 end
