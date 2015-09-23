87409ff4f798961b0c74c4d3ceaa84b50ed38ff6
Gerard Morera <grdmorera@gmail.com>
2015-09-23 18:39:02 +0100

    lines controller render json

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,7 +1,11 @@
 class LinesController < ApplicationController
   def create
-    Line.create
+    line = Line.create(product_ids: product_id)
+
+    render json: line
   end
 
-  
+  def product_id
+    params.permit(:product_id).values
+  end
 end
