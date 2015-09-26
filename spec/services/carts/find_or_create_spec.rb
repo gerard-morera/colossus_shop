b85a370f8098dfc5f09add10e1b5c13e413a1cce
Gerard Morera <grdmorera@gmail.com>
2015-09-26 12:28:34 +0100

    find_or_create test

---
+++ spec/services/carts/find_or_create_spec.rb
@@ -0,0 +1,31 @@
+module Carts
+  describe FindOrCreate do
+    
+    subject { FindOrCreate.new params }
+    
+    let(:params)  { double 'params', cart_id: value}
+    let(:Cart)    { double 'Cart' }
+    let(:cart_id) { value }
+    let(:create)  { create }
+
+    context "without an existing cart" do
+      let(:value) { nil }
+      
+      it "creates a new cart" do
+        allow(Cart).to receive(:create)
+
+        subject.call
+      end
+    end
+
+    context "with an existing cart" do 
+      let(:value) { 5 }
+      
+      it "fins the cart" do
+        allow(Cart).to receive(:find).with(cart_id)
+
+        subject.call
+      end
+    end
+  end
+end
